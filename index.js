7539e9a7512337474d37a476926ccf1308cafcf4
GeoReece000 <geofreytegeret@gmail.com>
2023-03-31 14:47:54 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function() { 
+   const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+   return drivers.slice(0,2)
+} 
+
+const returnLastTwoDrivers = function() { 
+    const drivers = ['Antonia', 'Nuru', 'Amari', 'Mo'];
+    return drivers.slice(2)
+ } 
+
+ const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier() {
+   return function fareMultiplier () {
+      const fare = 5
+
+      return fare * 5
+   }
+}
+
+
